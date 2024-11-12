07b854243c0e61d7aee3a8096e19af93f96608d4
Pavan Kumar Bestharapalli Tupakula (iDEAS-ERPA20371394D) <you@example.com>
2024-11-12 14:54:54 +0530

    HackerRank Java codes

---
+++ src/Java_stdin_stdout.java
@@ -0,0 +1,40 @@
+import java.util.*;
+/*Scanner scanner = new Scanner(System.in);
+String myString = scanner.next();
+int myInt = scanner.nextInt();
+scanner.close();
+
+System.out.println("myString is: " + myString);
+System.out.println("myInt is: " + myInt);
+
+Sample Input
+
+42
+100
+125
+Sample Output
+
+42
+100
+125
+
+
+
+ */
+
+
+
+public class Java_stdin_stdout {
+
+    public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            int a = scan.nextInt();
+            int b = scan.nextInt();
+            int c = scan.nextInt();
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+
+        }
+    }
+
